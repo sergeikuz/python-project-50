7c0e14d91364ac23e1382a729d149f1d36aecb81
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-06-04 21:05:19 +0300

    add dist gendiff

---
+++ gendiff/cli.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def get_information():
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    args = parser.parse_args()
+    return args
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+from gendiff.cli import get_information
+
+
+def main():
+    get_information()
+
+
+if __name__ == '__main__':
+    main()
