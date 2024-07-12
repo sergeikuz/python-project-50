5e3ffeaf64a99973b48a71aeaecaa4f8fbb2196a
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-12 18:28:00 +0300

    add generate_diff as diff and print(diff) to main()

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,13 @@
 
 
 from gendiff.cli import get_information
+from gendiff.generate_diff import generate_diff
 
 
 def main():
-    get_information()
-
+    args = get_information()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 if __name__ == '__main__':
     main()
