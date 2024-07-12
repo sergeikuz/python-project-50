cc90e09a030236f06dd4b1f0c63b6c9afb419967
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-12 18:26:39 +0300

    refactoring for 3rd step

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,9 +2,13 @@ import argparse
 
 
 def get_information():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(
+            description="Compares two configuration files and shows a difference."
+    )
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
-    parser.add_argument("-f", "--format", help="set format of output")
-    args = parser.parse_args()
-    return args
+    parser.add_argument(
+            "-f", "--format",
+            help="set format of output")
+
+    return parser.parse_args()
