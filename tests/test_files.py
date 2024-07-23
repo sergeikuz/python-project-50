415df18c8a6c637ad3ecdc1684d8e9fb1b4ca62d
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-23 14:50:04 +0300

    create test_files

---
+++ tests/test_files.py
@@ -0,0 +1,16 @@
+import os
+from gendiff.parser import parse_data_from_file
+
+def get_fixtures_path(name):
+    return os.path.join('tests/fixtures', name)
+
+
+file1_json = get_fixtures_path('file1.json')
+file1_yml = get_fixtures_path('file1.yml')
+file2_json = get_fixtures_path('file2.json')
+file2_yml = get_fixtures_path('file2.yml')
+
+
+def test_files_corresponds():
+    assert parse_data_from_file(file1_json) == parse_data_from_file(file1_yml)
+    assert parse_data_from_file(file2_json) == parse_data_from_file(file2_yml)
