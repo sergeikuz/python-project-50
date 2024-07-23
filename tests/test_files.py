7e8ed4254a91204e3a45bbaf09b44b48a714db17
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-23 16:32:45 +0300

    fix err by linter

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,6 +1,7 @@
 import os
 from gendiff.parser import parse_data_from_file
 
+
 def get_fixtures_path(name):
     return os.path.join('tests/fixtures', name)
 
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,12 +1,16 @@
 from gendiff.generate_diff import generate_diff
 import os
+import pytest
 
 
 def get_fixtures_path(name):
     return os.path.join('tests/fixtures', name)
 
 
-expected_diff = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+@pytest.fixture
+def expected_diff():
+    with open(get_fixtures_path('expected_diff.txt')) as exp:
+        return exp.read().strip()
 
 
 path1 = get_fixtures_path('file1.json')
