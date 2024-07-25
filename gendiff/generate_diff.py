3a7635ee11914cc40cf559b927fa2584c1ab7baa
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-25 17:54:42 +0300

    refuctoring generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,10 @@
 from gendiff.generator import diff
-
+from gendiff.parser import parse_data_from_file
+from gendiff.generator import stringify
 
 def generate_diff(file_path1, file_path2):
-    return diff(file_path1, file_path2)
+    dict1 = parse_data_from_file(file_path1)
+    dict2 = parse_data_from_file(file_path2)
+    diff_data = diff(dict1, dict2)
+    result = stringify(diff_data)
+    return result
