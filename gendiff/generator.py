3486f63b12eeb8e3b896c473c1e62c83924fa456
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-26 15:36:17 +0300

    fix func make_bool_string

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,14 +1,19 @@
-def make_str_bool_value_in_parse_data(path):
-    parsed_data = parse_data_from_file(path)
+def make_str_bool_value_in_parse_data(dict_d):
     fix_dict = {}
 
-    for k, v in parsed_data.items():
-        if isinstance(v, bool):
-            fix_dict[k] = str(v).lower()
+    for k, v in dict_d.items():
+        if isinstance(v, dict):
+            fix_dict[k] = make_str_bool_value_in_parse_data(v)
         else:
-            fix_dict[k] = v
+            if isinstance(v, bool):
+                fix_dict[k] = str(v).lower()
+            elif v == None:
+                fix_dict[k] = 'null'
+            else:
+                fix_dict[k] = v
 
     return fix_dict
+
 
 def stringify(value, replacer=' ', space_count=4, _lvl=1):
     if isinstance(value, dict):
@@ -17,10 +22,10 @@ def stringify(value, replacer=' ', space_count=4, _lvl=1):
         for el, val in value.items():
             if el.startswith(prefix):
                 result += f'{replacer * (space_count*_lvl - 2)}{el}: '
-                result += stringify(val, replacer, space_count, _lvl+1) + '\n'
+                result += stringify(val, replacer, space_count, _lvl + 1) + '\n'
             elif el:
                 result += f'{replacer * space_count * _lvl}{el}: '
-                result += stringify(val, replacer, space_count, _lvl+1) + '\n'
+                result += stringify(val, replacer, space_count, _lvl + 1) + '\n'
         result += replacer * space_count * (_lvl - 1) + '}'
     else:
         result = str(value)
