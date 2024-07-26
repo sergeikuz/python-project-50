8269064880bf02cac03a27e8436b89395cb5f2d7
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-26 21:52:45 +0300

    refuctoring

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,18 +1,19 @@
 def diff(dict1, dict2):
     keys = sorted(dict1.keys() | dict2.keys())
+
     diff_dict = {}
+
     for key in keys:
-        if key in dict1 and key in dict2 and isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+        if key not in dict2:
+            diff_dict[f'- {key}'] = dict1[key]
+        elif key not in dict1:
+            diff_dict[f'+ {key}'] = dict2[key]
+        elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
             diff_dict[f'  {key}'] = diff(dict1[key], dict2[key])
+        elif dict1[key] == dict2[key]:
+            diff_dict[f'  {key}'] = dict1[key]
         else:
-            if key not in dict2:
-                diff_dict[f'- {key}'] = dict1[key]
-            elif key not in dict1:
-                diff_dict[f'+ {key}'] = dict2[key]
-            elif dict1[key] == dict2[key]:
-                diff_dict[f'  {key}'] = dict1[key]
-            else:
-                diff_dict[f'- {key}'] = dict1[key]
-                diff_dict[f'+ {key}'] = dict2[key]
+            diff_dict[f'- {key}'] = dict1[key]
+            diff_dict[f'+ {key}'] = dict2[key]
 
     return diff_dict
