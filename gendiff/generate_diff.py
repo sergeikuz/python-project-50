57b2ca1a76f7386578ade3544cebed9c7feea911
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-26 21:52:20 +0300

    refuctoring diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,6 +3,7 @@ from gendiff.parser import parse_data_from_file
 from gendiff.formatter import stylish
 from gendiff.formatter import make_str_bool_value_in_parse_data
 
+
 def generate_diff(file_path1, file_path2, format='stylish'):
     dict1 = parse_data_from_file(file_path1)
     dict2 = parse_data_from_file(file_path2)
