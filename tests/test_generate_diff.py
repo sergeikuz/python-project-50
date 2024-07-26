f1ef5750eaa2a99e41cab46564fc3c5255d8c9a8
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-26 15:37:37 +0300

    fix fixtures expected_diff

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,16 +1,13 @@
 from gendiff.generate_diff import generate_diff
 import os
-import pytest
 
 
 def get_fixtures_path(name):
     return os.path.join('tests/fixtures', name)
 
 
-@pytest.fixture
-def expected_diff():
-    with open(get_fixtures_path('expected_diff.txt')) as exp:
-        return exp.read().strip()
+expected_diff = open(get_fixtures_path('expected_diff.txt')) 
+expected_diff = expected_diff.read().strip()
 
 
 path1 = get_fixtures_path('file1.json')
@@ -19,3 +16,4 @@ path2 = get_fixtures_path('file2.json')
 
 def test_generate_diff():
     assert expected_diff == generate_diff(path1, path2)
+print(test_generate_diff)
