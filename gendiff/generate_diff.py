74c1e46017588faa9908aba516e23b136a3f8130
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-26 15:37:04 +0300

    refuctoring solution by new func

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,12 @@
 from gendiff.generator import diff
 from gendiff.parser import parse_data_from_file
 from gendiff.generator import stringify
+from gendiff.generator import make_str_bool_value_in_parse_data
 
 def generate_diff(file_path1, file_path2):
     dict1 = parse_data_from_file(file_path1)
     dict2 = parse_data_from_file(file_path2)
     diff_data = diff(dict1, dict2)
-    result = stringify(diff_data)
+    change_values_in_dict = make_str_bool_value_in_parse_data(diff_data)
+    result = stringify(change_values_in_dict)
     return result
