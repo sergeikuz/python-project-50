d7b9bd4e13c488351342b46b2a2a434614e0c945
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-29 21:44:28 +0300

    refuctouring func

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,19 +1,63 @@
-def diff(dict1, dict2):
-    keys = sorted(dict1.keys() | dict2.keys())
+def for_deleted(key, value):
+    return {
+        'action': 'deleted',
+        'name': key,
+        'old_value': value
+    }
 
-    diff_dict = {}
+
+def for_added(key, value):
+     return {
+        'action': 'added',
+        'name': key,
+        'new_value': value
+    }
+
+
+def for_changed(key, value1, value2):
+     return {
+        'action': 'changed',
+        'name': key,
+        'old_value': value1,
+        'new_value': value2
+    }
+
+
+def for_unchanged(key, value):
+     return {
+        'action': 'unchanged',
+        'name': key,
+        'value': value
+    }
+
+
+def for_nested(key, value1, value2):
+    return {
+        'action': 'nested',
+        'name': key,
+        'value': generate(value1, value2)
+    }
+
+
+def generate(dict1, dict2):
+    keys = sorted(dict1.keys() | dict2.keys())
+    added_keys = dict2.keys() - dict1.keys()
+    deleted_keys = dict1.keys() - dict2.keys()
+
+    diff = []
 
     for key in keys:
-        if key not in dict2:
-            diff_dict[f'- {key}'] = dict1[key]
-        elif key not in dict1:
-            diff_dict[f'+ {key}'] = dict2[key]
-        elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
-            diff_dict[f'  {key}'] = diff(dict1[key], dict2[key])
-        elif dict1[key] == dict2[key]:
-            diff_dict[f'  {key}'] = dict1[key]
+        value1 = dict1.get(key)
+        value2 = dict2.get(key)
+        if key in added_keys:
+            diff.append(for_added(key, value2))
+        elif key in deleted_keys:
+            diff.append(for_deleted(key, value1))
+        elif isinstance(value1, dict) and isinstance(value2, dict):
+            diff.append(for_nested(key, value1, value2))
+        elif value1 != value2:
+            diff.append(for_changed(key, value1, value2))
         else:
-            diff_dict[f'- {key}'] = dict1[key]
-            diff_dict[f'+ {key}'] = dict2[key]
+            diff.append(for_unchanged(key, value1))
 
-    return diff_dict
+    return diff
