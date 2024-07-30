053bcb08e99f21bffd5d09f3d2954a1bb3b8b89f
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-30 22:53:59 +0300

    refuctoring

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,12 @@
 from gendiff.generator import generate
 from gendiff.parser import parse_data_from_file
-from gendiff.formatters.stylish import make_stylish
+from gendiff.formatters.choice_formatter import choice_format_diff
 
 
-def generate_diff(file_path1, file_path2, format='stylish'):
+def generate_diff(file_path1, file_path2, format_name='stylish'):
     dict1 = parse_data_from_file(file_path1)
     dict2 = parse_data_from_file(file_path2)
-    diff_data = generate(dict1, dict2)
-    result = make_stylish(diff_data)
+    diff = generate(dict1, dict2)
+    result = choice_format_diff(diff, format_name)
+
     return result
