fc72154840e88436f548517f33e8b2c4b338214f
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-30 15:42:40 +0300

    rename fixture and test name of stylish test

---
+++ tests/test_generate_diff_stylish.py
@@ -0,0 +1,18 @@
+from gendiff.generate_diff import generate_diff
+import os
+
+
+def get_fixtures_path(name):
+    return os.path.join('tests/fixtures', name)
+
+
+expected_diff = open(get_fixtures_path('expected_stylish.txt'))
+expected_diff = expected_diff.read().strip()
+
+
+path1 = get_fixtures_path('file1.json')
+path2 = get_fixtures_path('file2.json')
+
+
+def test_generate_diff():
+    assert expected_diff == generate_diff(path1, path2)
