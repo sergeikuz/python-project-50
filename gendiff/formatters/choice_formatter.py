3d145d614ac84f08adeb2608606062703c8620df
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-07-30 22:55:04 +0300

    create func with options to choice format diff

---
+++ gendiff/formatters/choice_formatter.py
@@ -0,0 +1,14 @@
+from gendiff.formatters.json import make_json
+from gendiff.formatters.plain import make_plain
+from gendiff.formatters.stylish import make_stylish
+
+
+def choice_format_diff(diff, format_name):
+    if format_name == 'stylish':
+        return make_stylish(diff)
+    if format_name == 'plain':
+        return make_plain(diff)
+    if format_name == 'json':
+        return make_json(diff)
+    else:
+        raise ValueError(f"Unsupported formatter: {format_name}")
