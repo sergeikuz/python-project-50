b9c24f53982440a658e728b04ba59c2ff2810c07
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-08-12 18:11:24 +0300

    refactoring if/elif-> math and adding variable descriptions

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -15,27 +15,27 @@ def to_str(data):
     return fix_dict
 
 
-def make_data_stylish(data):
+def make_data_stylish(data: list) -> dict:
     diff = {}
     for item in data:
-        for k, v in item.items():
-            if v == 'nested':
+        match item.get('action'):
+            case 'nested':
                 diff[f'  {item["name"]}'] = make_data_stylish(item['value'])
-            elif v == 'unchanged':
+            case 'unchanged':
                 diff[f'  {item["name"]}'] = item['value']
-            elif v == 'deleted':
+            case 'deleted':
                 diff[f'- {item["name"]}'] = item['old_value']
-            elif v == 'added':
+            case 'added':
                 diff[f'+ {item["name"]}'] = item['new_value']
-            elif v == 'changed':
+            case 'changed':
                 diff[f'- {item["name"]}'] = item['old_value']
                 diff[f'+ {item["name"]}'] = item['new_value']
-            #elif v not in ['unchanged', 'deleted', 'added', 'changed']:
-                #raise ValueError(f"Unsupported action: {v}")
+#            #elif v not in ['unchanged', 'deleted', 'added', 'changed']:
+#                #raise ValueError(f"Unsupported action: {v}")
     return diff
 
 
-def make_str(fix_dict, replacer=' ', space_count=4, _lvl=1):
+def make_str(fix_dict: dict, replacer=' ', space_count=4, _lvl=1) -> str:
     if isinstance(fix_dict, dict):
         prefix = ('  ', '+ ', '- ')
         result = '{\n'
@@ -52,7 +52,7 @@ def make_str(fix_dict, replacer=' ', space_count=4, _lvl=1):
     return result
 
 
-def make_stylish(data):
+def make_stylish(data: list) -> str:
     stylish_data = make_data_stylish(data)
     stylish_data_with_correct_value = to_str(stylish_data)
     result = make_str(stylish_data_with_correct_value)
