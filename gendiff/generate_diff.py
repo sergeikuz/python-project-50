ad6a00fbf03c0a4c150a3e6b5e654a71f3bd0e2a
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2024-08-22 17:31:47 +0300

    rename func() choice-> choose

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,12 +1,12 @@
 from gendiff.generator import get_differences
 from gendiff.parser import parse_data_from_file
-from gendiff.formatters.choice_formatter import choice_format_diff
+from gendiff.formatters.choose_formatter import choose_format_diff
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     dict1 = parse_data_from_file(file_path1)
     dict2 = parse_data_from_file(file_path2)
     diff = get_differences(dict1, dict2)
-    result = choice_format_diff(diff, format_name)
+    result = choose_format_diff(diff, format_name)
 
     return result
